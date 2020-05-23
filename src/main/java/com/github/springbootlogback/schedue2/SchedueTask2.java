38b527bc2acbf975247b711dd5cc84d9986ce1cd
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-05-23 18:08:18 +0800

    init commit

---
+++ src/main/java/com/github/springbootlogback/schedue2/SchedueTask2.java
@@ -0,0 +1,30 @@
+package com.github.springbootlogback.schedue2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * 创建时间为 11:26 2019-06-12
+ * 项目名称 spring-boot-logback
+ * </p>
+ *
+ * @author 石少东
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+
+
+@Slf4j
+//@Service
+public class SchedueTask2 {
+
+    @Scheduled(fixedRate = 1000)
+    public void task() {
+        log.info("schedue2---info:{}", new Date().toString());
+    }
+
+}
